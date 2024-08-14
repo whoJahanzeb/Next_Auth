40b96d86a40726998c870c74ae180ec27eb0d999
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-14 12:28:37 +0500

    set mailers

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,6 +1,30 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async ({ email, emailType, userId }) => {
+export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
-  } catch (error) {}
+    //TODO: configure mail for usage
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    const mailOption = {
+      from: "jahanzeb@jahanzeb.ai",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email." : "Reset your password.",
+      html: "<b>Hello world?</b>",
+    };
+
+    const mailResonse = await transporter.sendMail(mailOption);
+    return mailResonse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
